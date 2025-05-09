37da0602302ff0bb76f45b983add603512b7bbf8
TUDONG05 <khactu731@gmail.com>
2025-05-09 08:29:28 +0700

    danh sách móc nối_ bài thực hành buổi 5

--- th1/main.cpp
+++ th1/main.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
-#include<string>
 #include<iomanip>
-using namespace std;
+#include<string>
+
 #define MAX 100
+
+using namespace std;
+
 struct SinhVien
 {
     string maSV;
-    string ho;
+    string hoDem;
     string ten;
     string gioiTinh;
     int namSinh;
-    float diem;
+    float diemTK;
 };
 
 struct vector
@@ -18,138 +21,174 @@ struct vector
     int count;
     SinhVien sv[MAX];
 };
-
-int empty(vector v)
-{
-    return (v.count==-1);
-}
-void creat (vector &v)
+void creat(vector &v)
 {
     v.count=-1;
 }
-void xoa(vector &v)
+int empty(vector v)
 {
-    if(empty(v))
-        cout<<"Mang rong!";
-    else
-    {
-        for(int i=1;i<=v.count;i++)
-            v.sv[i-1]=v.sv[i];
-    }
-    v.count--;
+    return (v.count ==-1);
 }
+
+int full(vector v)
+{
+    return (v.count==MAX-1);
+}
+
 void nhapSV(SinhVien &sv)
 {
-    cout<<"MaSV:";
-    cin.ignore();
+    
+    cout<<"Ma sv:";
     getline(cin,sv.maSV);
 
     cout<<"Ho dem:";
-    getline(cin, sv.ho);
+    getline(cin,sv.hoDem);
 
-    cout<<"ten:";
+    cout<<"Ten:";
     getline(cin,sv.ten);
 
-    cout<<"GIoi tinh";
+    cout<<"Gioi tinh:";
     getline(cin,sv.gioiTinh);
 
-    cout<<"nam sinh:";
+    cout<<"Nam sinh:";
     cin>>sv.namSinh;
 
-    cout<<"Diem tong ket:";
-    cin>>sv.diem;
-
+    cout<<"Diem TK:";
+    cin>>sv.diemTK;
 
 }
 
-void add(vector &v,SinhVien sv ){
-    if(v.count==MAX-1)
-        cout<<"Danh sach day!";
+void add(vector &v, SinhVien sv)
+{
+    if(full(v))
+        cout<<"Danh sach da day!"<<endl;
     else
     {
         v.count++;
-        v.sv[v.count]= sv;
+        v.sv[v.count] = sv;
     }
+        
 }
+
+void addSV(vector &v, string maSV, string hoDem, string ten, string gioiTinh,int  namSinh, float diemtk)
+{
+    SinhVien sv;
+    sv.maSV = maSV;
+    sv.hoDem =hoDem;
+    sv.ten = ten;
+    sv.gioiTinh = gioiTinh;
+    sv.namSinh= namSinh;
+    sv.diemTK = diemtk;
+
+    if(full(v))
+        cout<<"DS day!"<<endl;
+    else
+        {
+            v.count++;
+            v.sv[v.count] = sv;
+        }
+}
+
 void inDS(vector v)
 {
-    cout<<"-------------------------Danh sach sinh vien------------------------------"<<endl;
-    cout<<left<<setw(15)<<"Ma sv"<<setw(15)<<"Ho dem"<<setw(15)<<"ten"<<setw(15)<<"Gioi tinh"<<setw(10)<<"nam sinh"<<"Diem tk"<<endl;
+    if(empty(v))
+        cout<<"Danh sach rong!"<<endl;
+    else
+    {   cout<<left<<setw(10)<<"MaSV"
+                  <<setw(15)<<"Ho dem"
+                  <<setw(10)<<"Ten"
+                  <<setw(15)<<"Gioi tinh"
+                  <<setw(10)<<"Nam sinh"
+                  <<setw(10)<<"Diem TK"<<endl;
+        for(int i=0;i<=v.count;i++)
+        cout<<left<<setw(10)<<v.sv[i].maSV
+                    <<setw(15)<<v.sv[i].hoDem
+                    <<setw(10)<<v.sv[i].ten
+                    <<setw(15)<<v.sv[i].gioiTinh
+                    <<setw(10)<<v.sv[i].namSinh
+                    <<setw(10)<<v.sv[i].diemTK<<endl;
+        cout<<"================================================================="<<endl;
 
-    for(int i =0;i<=v.count;i++)
-         cout<<left<<setw(15)<<v.sv[i].maSV<<setw(15)<<v.sv[i].ho<<setw(15)<<v.sv[i].ten<<setw(15)<<v.sv[i].gioiTinh<<setw(10)<<v.sv[i].namSinh<<v.sv[i].diem<<endl;
-
+    }
 }
-void insert(vector &v,SinhVien sv, int k)
+
+void del(vector &v)
 {
-    if(v.count==MAX-1)
-        cout<<"ds day roi!";
+    if(empty(v))
+        cout<<"Danh sach rong!"<<endl;
     else
     {
-        for(int i=v.count;i>=k-1;i--)
-            v.sv[i+1]= v.sv[i];
-            v.sv[k-1]=sv;
-            v.count++;
+        for(int i=0;i<v.count;i++)
+            v.sv[i]=v.sv[i+1];
+        v.count--;
+        cout<<"Xoa thanh cong"<<endl;
+    }
+    
+}
+
+void chen(vector &v,string maSV, string hoDem, string ten, string gioiTinh,int  namSinh, float diemtk,int k)
+{
+    if(full(v))
+        cout<<"Danh sach day!"<<endl;
+    else
+    {
+        
+        for(int i=v.count+1;i>=k-1;i--)
+            v.sv[i+1] = v.sv[i];
+
+        v.sv[k-1].maSV= maSV;
+        v.sv[k-1].hoDem=hoDem;
+        v.sv[k-1].ten=ten;
+        v.sv[k-1].gioiTinh=gioiTinh;
+        v.sv[k-1].namSinh=namSinh;
+        v.sv[k-1].diemTK =diemtk;
+        v.count++;
+        cout<<"chen thanh cong!"<<endl;
+
     }
 }
+
+
+// sap chon de sap ten
 void sapTen(vector &v)
 {
-    for(int i=0;i<v.count;i++)
-        for(int j=v.count;j>i;j--)
-            if(v.sv[j].ten > v.sv[j-1].ten)
-                {
-                    SinhVien tg = v.sv[j];
-                    v.sv[j]= v.sv[j-1];
-                    v.sv[j-1]=tg;
-                }
-}
-// void sapTen(vector &v)
-// {
-//     for (int i = 0; i < v.count; i++)
-//         for (int j = v.count; j > i; j--)
-//             if (v.sv[j].ten < v.sv[j-1].ten) // SẮP XẾP TĂNG DẦN
-//             {
-//                 SinhVien tg = v.sv[j];
-//                 v.sv[j] = v.sv[j-1];
-//                 v.sv[j-1] = tg;
-//             }
-// }
-
-
-
-
-
-
-int main(){
-    vector v;
-    creat(v);
-
-    cout<<"nhap so sv:";
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<=v.count-1;i++)
     {
-        SinhVien s1;
-        nhapSV(s1);
-        add(v,s1);
-
+        int m =i;
+        for(int j=i+1;j<=v.count;j++)
+        if(v.sv[j].ten<v.sv[m].ten)
+            m = j;
+        SinhVien tg = v.sv[i];
+        v.sv[i]= v.sv[m];
+        v.sv[m]= tg;
     }
     
+    
+}
+int main()
+{
+    vector v;
+    creat(v);
+    
+    addSV(v,"SV1001" ,"Tran Van","Thanh", "Nam", 1999 ,7.5);
+    addSV(v,"SV1002", "Nguyen Thi","Huong" ,"Nu", 2000 ,7.3);
+    addSV(v,"SV1003", "Nguyen Van", "Binh", "Nam" ,1998 ,6.4);
+    addSV(v,"SV1004","Bui Thi","Hong","Nu",2000,6.4);
+    addSV(v,"SV1005","Duong Van","Giang","Nam",1998,8.3);
+    
+    cout<<"====================Danh sach sinh vien=========================="<<endl;
     inDS(v);
-    // xoa(v);
-    // inDS(v);
 
-    // SinhVien s2;
+    del(v);
+    cout<<"==============Danh sach sinh vien sau khi xoa===================="<<endl;
+    inDS(v);
 
-    // nhapSV(s2);
-    // insert(v,s2,3);
-
-    // inDS(v);
+    chen(v,"SV1006","Le Thi","Doan","Nu",1998,7.6,3);
+    cout<<"==============Danh sach sinh vien sau khi chen==================="<<endl;
+    inDS(v);
 
     sapTen(v);
+    cout<<"==============Danh sach sau khi sap theo ten====================="<<endl;
     inDS(v);
-    
-
-
 
 }
